3a1ac842b1440b1a459ffffe4329064b9effade7
Naveen G <naveeng2402@gmail.com>
2024-08-03 13:30:05 +0530

    feat: add lucia auth

---
+++ server/middleware/cors.ts
@@ -0,0 +1,5 @@
+export default defineEventHandler((event) => {
+  handleCors(event, {
+    methods: "*",
+  });
+});
---
+++ server/utils/lucia.ts
@@ -0,0 +1,50 @@
+import { Lucia } from "lucia";
+import type { H3Event } from "h3";
+import { DrizzlePostgreSQLAdapter } from "@lucia-auth/adapter-drizzle";
+
+const adapter = new DrizzlePostgreSQLAdapter(
+  useDrizzle(),
+  authSessionSchema,
+  authUserSchema,
+);
+
+export const lucia = new Lucia(adapter, {
+  sessionCookie: {
+    attributes: {
+      secure: !import.meta.dev,
+    },
+  },
+  getUserAttributes: (attributes) => ({
+    username: attributes.username,
+    role: attributes.role,
+    email: attributes.email,
+    organizationId: attributes.organizationId,
+  }),
+});
+
+export const isValidUser = (event: H3Event, role: string) => {
+  if (event.context.user?.role === role) return true;
+
+  return sendError(
+    event,
+    createError({
+      statusCode: 401,
+      statusMessage: "Unauthorized",
+      message: "Unauthorized",
+    }),
+  );
+};
+
+declare module "lucia" {
+  interface Register {
+    Lucia: typeof lucia;
+    DatabaseUserAttributes: DatabaseUserAttributes;
+  }
+}
+
+interface DatabaseUserAttributes {
+  username: string;
+  role: string;
+  email: string;
+  organizationId: string;
+}
