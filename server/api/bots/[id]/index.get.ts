cbfb17334fc10afcfc3b9514e9c3521124785e5c
Naveen G <naveeng2402@gmail.com>
2024-08-07 13:57:08 +0530

    fix: redundant imports

--- server/api/bots/[id]/index.get.ts
+++ server/api/bots/[id]/index.get.ts
@@ -1,8 +1,4 @@
-import { getBotDetails } from "~/server/utils/db/bot";
-import { isValidQueryHandler } from "~/server/utils/validations";
-
 export default defineEventHandler(async (event) => {
-  await isValidQueryHandler(event, checkPayloadId("organization_id"));
   const { id: botId } = await isValidRouteParamHandler(
     event,
     checkPayloadId("id"),
