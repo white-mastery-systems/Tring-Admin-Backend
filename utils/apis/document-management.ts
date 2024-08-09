5b0612c4f6e428d5cb35b5bfb3cec21c0dd0af4c
Naveen G <naveeng2402@gmail.com>
2024-08-09 17:13:07 +0530

    feat: add document api connections

--- utils/apis/document-management.ts
+++ utils/apis/document-management.ts
@@ -1,4 +1,41 @@
 export const listDocumentsByBotId = async (botId: string) => {
   const documentsList = await $fetch(`/api/bots/${botId}/documents`);
-  return documentsList;
+  return documentsList.map((document) => ({
+    ...document,
+    createdAt: formatDate(new Date(document.createdAt), "dd.MM.yyyy"),
+  }));
+};
+
+export const deployDocument = async (botId: string, documentId: string) => {
+  await $fetch(`/api/bots/${botId}/documents/${documentId}/deploy`);
+};
+
+export const viewDocument = async (botId: string, documentId: string) => {
+  navigateTo(`/api/bots/${botId}/documents/${documentId}`, {
+    external: true,
+    open: {
+      target: "_blank",
+    },
+  });
+};
+
+export const deleteDocument = async (botId: string, documentId: string) => {
+  await $fetch(`/api/bots/${botId}/documents/${documentId}`, {
+    method: "DELETE",
+  });
+};
+
+export const createDocument = async (
+  botId: string,
+  document: { name: string; files: File },
+) => {
+  const form = new FormData();
+
+  form.append("name", document.name);
+  form.append("files", document.files);
+
+  await $fetch(`/api/bots/${botId}/documents`, {
+    method: "POST",
+    body: form,
+  });
 };
