fb21c8b7d6f2580ce00ce7de5e416a67ea9d8c4e
Naveen G <naveeng2402@gmail.com>
2024-08-10 12:50:50 +0530

    feat: add preview button

--- pages/BotManagementDetails/[id].vue
+++ pages/BotManagementDetails/[id].vue
@@ -7,44 +7,64 @@
       <div class="list-header-align">
         <div class="header-content-align">
           <div class="items-cetner flex gap-4">
-            <span class="content-align text-[17px] font-bold">Yourstore Bot-4</span>
-            <div class="acive_class">
+            <span class="content-align text-[17px] font-bold">{{
+              botDetails.name
+            }}</span>
+            <!-- {{ botDetails }}
+            {{ botDetails.documentId }} -->
+            <div v-if="botDetails.documentId" class="acive_class">
               <div class="active-circle-align rounded-full"></div>
               <span class="text-[14px]">Active</span>
             </div>
+            <!-- v-else -->
+            <div v-else class="deacive_class pl-2 font-medium">
+              <div class="deactive-circle-align rounded-full"></div>
+              <span>Inactive</span>
+            </div>
           </div>
           <div class="flex items-center gap-4">
-            <span class="text-[17px] font-bold text-black">Date Created:
-              <span class="text-[15px] font-medium text-black">12.09.2013</span></span>
-            <span>
-              <button class="button-align text-[14px] font-medium">
-                Deactivate Bot
-              </button>
+            <span class="text-[17px] font-bold text-black"
+              >Date Created:
+              <span class="text-[15px] font-medium text-black">{{
+                dateFormate
+              }}</span>
+            </span>
+            <span v-if="botDetails.documentId" class="flex gap-4">
+              <Button
+                class="button-align text-[14px] font-medium"
+                @click="deactivateBot(paramId.params.id)"
+                >Deactivate Bot</Button
+              >
+              <UiButton
+                as="a"
+                :href="previewUrl"
+                target="_blank"
+                class="bg-[#474df9] text-[14px] font-medium text-white hover:bg-[#474df9] hover:brightness-90"
+                >Preview Bot</UiButton
+              >
             </span>
           </div>
           <!-- <span class="font-semibold content-align">Date Created</span>
           <span class="font-semibold content-align">Status</span> -->
         </div>
       </div>
-      <div class="bot-list-align text-[15px]" v-for="(list, index) in botList" :key="index"
-        @click="botManagementDetails(list)">
+      <div
+        class="bot-list-align text-[14px]"
+        v-for="(list, index) in dataList"
+        :key="index"
+        @click="botManagementDetails(list, index)"
+      >
         <div class="list_align">
-          <span class="bot_name_align font-medium">{{ list.name }}</span>
-          <span class="createAt_align font-medium text-black"
-            :style="{ 'padding-inline-end': !list.status ? '110px' : '123px' }">{{ list.createdAt }}</span>
-          <div v-if="list.status" class="acive_class font-medium">
-            <div class="active-circle-align rounded-full"></div>
+          <span class="bot_name_align font-medium">{{ list.bot }}</span>
+          <!-- <span class="font-medium pr-14">{{ list.createAt }}</span> -->
+          <!-- <div v-if="list.status" class="pr-3 acive_class font-medium">
+            <div class="rounded-full active-circle-align"></div>
             <span>Active</span>
           </div>
-          <div v-else class="deacive_class pl-2 font-medium">
-            <div class="deactive-circle-align rounded-full"></div>
+          <div v-else class="pl-2 deacive_class font-medium">
+            <div class="rounded-full deactive-circle-align"></div>
             <span>Inactive</span>
-          </div>
-          <!-- v-if="!list.arrowChange" -->
-          <div class="pr-4">
-            <!-- <img src="assets\icons\left_arrow.svg" width="30"> -->
-            <LeftArrowIcon class="arrow-aling hover:text-[#ffbc42]" />
-          </div>
+          </div> -->
         </div>
         <div>
           <LeftArrowIcon class="arrow-aling hover:text-[#ffbc42]" />
@@ -60,148 +80,213 @@
   </div>
 </template>
 <script setup lang="ts">
-import { ref } from "vue";
-const selectedValue = ref("Today");
-const route = useRoute("BotManagementDetails-id");
-const botList = await listApiBots()
-const dataList = ref([
-  {
-    _id: 1,
-    bot: "UI Customization",
-    arrowChange: false,
-    routeName: 'UiCustomization-id',
-  },
-  {
-    _id: 2,
-    bot: "CRM Integration",
-    arrowChange: false,
-    routeName: '',
-  },
-  {
-    _id: 3,
-    bot: "Bot Configuration",
-    arrowChange: false,
-    routeName: "CreateBot-id",
+  import { ref } from "vue";
+  const selectedValue = ref("Today");
+  const route = useRoute();
+  const paramId: any = route;
+  const botDetails: any = await getBotDetails(paramId.params.id);
+  const dataList = ref([
+    {
+      _id: 1,
+      bot: "UI Customization",
+      routeName: "UiCustomization-id",
+    },
+    {
+      _id: 2,
+      bot: "CRM Integration",
+      routeName: "",
+    },
+    {
+      _id: 3,
+      bot: "Bot Configuration",
+      routeName: "CreateBot-id",
+    },
+    {
+      _id: 4,
+      bot: "Document Management",
+      routeName: "BotDocumentManagement-id",
+    },
+  ]);
+  const previousIndex = ref(0);
+  const menuList = ref([
+    {
+      content: "Today",
+      value: "Today",
+    },
+    {
+      content: "Weekly",
+      value: "Weekly",
+    },
+    {
+      content: "Monthly",
+      value: "Monthly",
+    },
+    {
+      content: "Quarterly",
+      value: "Quarterly",
+    },
+    {
+      content: "Halfyearly",
+      value: "Halfyearly",
+    },
+    {
+      content: "Yearly",
+      value: "Yearly",
+    },
+  ]);
 
-  },
-  {
-    _id: 4,
-    bot: "Document Management",
-    arrowChange: false,
-    routeName: 'BotDocumentManagement-id'
-  },
-]);
+  const dateFormate = computed(() => {
+    if (botDetails && botDetails.createdAt) {
+      return formatDateStringToDate(botDetails.createdAt);
+    }
+    return null;
+  });
 
+  const previewUrl = computed(() => {
+    let col = botDetails.metadata.ui.color as string;
+    col = col
+      .split(" ")
+      .map((element) => {
+        if (element.at(-1) === "%") return element.slice(0, -1);
+        else return element;
+      })
+      .join(" ");
+    return `https://tring-databot.pripod.com/?orgname=WMS&chatbotid=${paramId.params.id}&brandcolor=${
+      col
+    }`;
+  });
+  // onMounted(async () => {
+  //   console.log(paramId.params.id, "paramId")
+  //   try {
+  //     botDetails.value =
+  //   } catch (error) {
+  //     console.error("Error fetching bot details:", error);
+  //   }
+  // })
 
-
-const botManagementDetails = async (list: any) => {
-  return navigateTo({
-    name: list.routeName,
-    params: { id: route.params.id },
-  })
-}
+  const botManagementDetails = async (list: any, index: any) => {
+    // console.log(list.bot.trim().toLowerCase().replace(/\s+/g, ' ') , "list")
+    // if (list.bot === dataList.value[index].bot) {
+    await navigateTo({
+      name: list.routeName,
+      params: { id: paramId.params.id },
+    });
+    // }
+  };
+  const deactivateBot = async () => {
+    await disableBot(paramId.params.id);
+    await getBotDetails(paramId.params.id);
+  };
 </script>
 
 <style scoped>
-.bot-manage-main-container {
-  padding: 8px 25px;
-}
+  .bot-manage-main-container {
+    padding: 8px 25px;
+  }
 
-.header-align {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  font-family: segoe UI Regular;
-}
+  .header-align {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-family: segoe UI Regular;
+  }
 
-.bot-main-align {
-  margin-top: 30px;
-}
+  .bot-main-align {
+    margin-top: 30px;
+  }
 
-.list_align {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 65%;
-  /* background: rgba(255, 255, 255, 1); */
-  /* padding: 30px 30px; */
-  /* box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important; */
-  border-radius: 10px;
-  /* gap: 100px; */
-  /* margin: 10px 0; */
-}
+  .list_align {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 65%;
+    /* background: rgba(255, 255, 255, 1); */
+    /* padding: 30px 30px; */
+    /* box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important; */
+    border-radius: 10px;
+    /* gap: 100px; */
+    /* margin: 10px 0; */
+  }
 
-.bot-list-align {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  display: flex;
-  align-items: center;
-  background: rgba(255, 255, 255, 1);
-  padding: 20px 30px;
-  width: 100% !important;
-  box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important;
-  border-radius: 10px;
-  margin: 20px;
-}
+  .bot-list-align {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    display: flex;
+    align-items: center;
+    background: rgba(255, 255, 255, 1);
+    padding: 20px 30px;
+    width: 100% !important;
+    box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important;
+    border-radius: 10px;
+    margin: 20px;
+  }
 
-.acive_class {
-  display: flex;
-  align-items: center;
-  /* width: 50px; */
-  color: rgba(26, 187, 0, 1) !important;
-  gap: 5px;
-}
+  .acive_class {
+    display: flex;
+    align-items: center;
+    /* width: 50px; */
+    color: rgba(26, 187, 0, 1) !important;
+    gap: 5px;
+  }
 
-/* .deacive_class {
+  /* .deacive_class {
   display: flex;
   align-items: center;
   gap: 5px;
   color: rgba(255, 0, 0, 1);
 } */
 
-.active-circle-align {
-  display: flex;
-  align-items: center;
-  background-color: rgba(26, 187, 0, 1);
-  width: 6px;
-  height: 6px;
-}
-
-.header-content-align {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  width: 100%;
-}
-
-.list-header-align {
-  padding: 10px 0px 10px 20px;
-  display: flex;
-  align-items: center;
-  /* justify-content: space-between; */
-  width: 100%;
-  /* gap: 100px; */
-  border-bottom: 0.5px solid rgba(181, 181, 181, 1);
-}
-
-.button-align {
-  background-color: red;
-  color: white;
-  border-radius: 8px;
-  padding: 10px 10px;
-}
-
-.bot-list-align:hover {
-  color: rgba(255, 188, 66, 1);
-  background: rgba(255, 248, 235, 1) !important;
-}
-
-.arrow-aling {
-  width: 30px;
-}
-
-/* .right-dropdown-align {
+  .active-circle-align {
+    display: flex;
+    align-items: center;
+    background-color: rgba(26, 187, 0, 1);
+    width: 6px;
+    height: 6px;
+  }
+  .header-content-align {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    width: 100%;
+  }
+  .list-header-align {
+    padding: 10px 0px 10px 20px;
+    display: flex;
+    align-items: center;
+    /* justify-content: space-between; */
+    width: 100%;
+    /* gap: 100px; */
+    border-bottom: 0.5px solid rgba(181, 181, 181, 1);
+  }
+  .button-align {
+    background-color: red;
+    color: white;
+    border-radius: 8px;
+    padding: 10px 10px;
+  }
+  .bot-list-align:hover {
+    color: rgba(255, 188, 66, 1);
+    background: rgba(255, 248, 235, 1) !important;
+  }
+  .arrow-aling {
+    width: 30px;
+  }
+  .deacive_class {
+    display: flex;
+    align-items: center;
+    gap: 5px;
+    color: rgba(255, 0, 0, 1);
+    padding-inline-end: 201px;
+  }
+  .deactive-circle-align {
+    display: flex;
+    align-items: center;
+    background-color: rgba(255, 0, 0, 1);
+    width: 6px;
+    height: 6px;
+  }
+  /* .right-dropdown-align {
   display: flex;
   align-items: center;
   background: rgba(255, 255, 255, 1);
@@ -210,7 +295,7 @@ const botManagementDetails = async (list: any) => {
   box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important;
   border-radius: 10px;
 } */
-/* .deactive-circle-align {
+  /* .deactive-circle-align {
   display: flex;
   align-items: center;
   background-color: rgba(255, 0, 0, 1);
@@ -218,7 +303,7 @@ const botManagementDetails = async (list: any) => {
   height: 5px;
 } */
 
-/* .bot_name_align {
+  /* .bot_name_align {
   width: 11%;
   white-space: nowrap;
   overflow: hidden;
