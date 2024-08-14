13c94015fd08d02494ff2c53f09ad6d76b0a6ca5
Purushothaman R <purushothamanramesh3@gmail.com>
2024-08-14 12:39:05 +0530

    feat: add confirm modal component

---
+++ components/ConfirmationModal.vue
@@ -0,0 +1,34 @@
+<script setup lang="ts">
+  interface Props {
+    title: string;
+    description: string;
+  }
+
+  const props = withDefaults(defineProps<Props>(), {
+    title: "",
+    description: "",
+  });
+
+  const emit = defineEmits<{ (e: "confirm"): void }>();
+
+  const open = defineModel<boolean>("open", { default: false, required: true });
+</script>
+
+<template>
+  <UiAlertDialog v-model:open="open">
+    <UiAlertDialogContent>
+      <UiAlertDialogHeader>
+        <UiAlertDialogTitle>{{ title }}</UiAlertDialogTitle>
+        <UiAlertDialogDescription>
+          {{ description }}
+        </UiAlertDialogDescription>
+      </UiAlertDialogHeader>
+      <UiAlertDialogFooter>
+        <UiAlertDialogCancel>Cancel</UiAlertDialogCancel>
+        <UiAlertDialogAction @click="$emit('confirm')" class="bg-[#474df9] text-white hover:bg-[#474df9] hover:brightness-90"
+          >Confirm</UiAlertDialogAction
+        >
+      </UiAlertDialogFooter>
+    </UiAlertDialogContent>
+  </UiAlertDialog>
+</template>
