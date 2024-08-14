ea625e1497dba3c182983c60af80da6ef648e787
Naveen G <naveeng2402@gmail.com>
2024-08-14 16:17:06 +0530

    fix: chat transcript page

--- pages/AnalyticsLeadsInfo/[id].vue
+++ pages/AnalyticsLeadsInfo/[id].vue
@@ -6,19 +6,14 @@
           <button @click="router.back()">
             <img src="assets\icons\right_arrow.svg" width="20"></img>
           </button>
-          <span class="font-bold text-lg">Yourstore Bot-1</span>
+          <span class="font-bold text-lg">{{ leadData?.botUser?.name  }}</span>
         </div>
         <span class="font-bold text-lg text-[16px]"></span>
-      </div>
-      <div class="flex items-center space-x-4">
-        <span>
-          <img src="assets\icons\export_btn.svg" width="120">
-        </span>
       </div>
     </div>
   </div>
   <!-- <template> -->
-  <div class="flex justify-between">
+  <div class="flex justify-around gap-8 mx-8">
     <!-- <div> -->
     <!-- <div>{{ getLeadTranscriptList[0].metadata }}</div> -->
     <UiTabs default-value="Client Info" class="w-[250px]">
@@ -30,75 +25,54 @@
           Campaign info
         </UiTabsTrigger>
       </UiTabsList>
-      <div v-for="(infoList, index) in getLeadTranscriptList" :key="index">
         <UiTabsContent value="Client Info">
           <div class="client_info_align gap-2">
-            <span class="font-medium">Os: <span class="font-bold">{{ infoList.metadata.os }}</span></span>
-            <span class="font-medium">Browser: <span class="font-bold">{{ infoList.metadata.browser }}</span></span>
-            <span class="font-medium">IP Address: <span class="font-bold">{{ infoList.metadata.ipAddress
-                }}</span></span>
-            <!-- <span class="font-medium">Continent Code: <span class="font-bold">{{ }}</span></span>
-            <span class="font-medium">Continent Name: <span class="font-bold">{{ }}</span></span>
-            <span class="font-medium">Country Code: <span class="font-bold">{{ }}</span></span> -->
-            <span class="font-medium">Country Name: <span class="font-bold">{{ infoList.metadata.country
-                }}</span></span>
-            <span class="font-medium">State Prov: <span class="font-bold">{{ infoList.metadata.state }}</span></span>
-            <span class="font-medium">City: <span class="font-bold">{{ infoList.metadata.city }}</span></span>
+            <span v-for="[key, value] in details[0]" class="font-medium">{{ key }}: <span class="font-bold">{{ value }}</span></span>
           </div>
         </UiTabsContent>
         <UiTabsContent value="Campaign info">
-          {{ (Object.entries(infoList.metadata.params).length) ? infoList.metadata.params : '' }}
+          <div class="client_info_align gap-2">
+            <span v-for="[key, value] in details[1]" class="font-medium">{{ key }}: <span class="font-bold">{{ value }}</span></span>
+          </div>
         </UiTabsContent>
-      </div>
     </UiTabs>
     <!-- </div> -->
     <!-- {{ getLeadTranscriptList[0].messages.map((item: any) ) }} -->
     <div class="chatinfo-align rounded-lg">
       <div class="flex items-center font-medium justify-between chat-header-align">
         <div class="flex items-center gap-2">
-          <div class="rounded-full profile-align"></div>
-          <span>
-            Name
+          <!-- <div class="rounded-full profile-align"></div> -->
+          <span class=text-[14px]>
+            {{ leadData?.bot?.name }}
           </span>
         </div>
-        <div>
-          <img src="assets/icons/chat_menu.svg" width="30" height="30" />
-        </div>
+          <!-- <div>
+            <img src="assets/icons/chat_menu.svg" width="30" height="30" />
+          </div> -->
       </div>
       <div class="all-message-align">
-        <div class="chat-container-align" v-for="(messageList, messageIndex) in getLeadTranscriptList[0].messages"
+        <div class="chat-container-align" v-for="(messageList, messageIndex) in leadData?.messages.slice(1)"
           :key="messageIndex">
           <!-- {{ messageList }} -->
           <div class="message-left-align" v-if="messageList.role === 'user'">
-            <div class="text-[14px]" style="color: #8A8A8A">
-              you
-            </div>
+            <span class="text-[14px]" style="color: #8A8A8A">
+              {{ leadData?.botUser?.name }}
+            </span>
             <div class="flex justify-center items-end current-user rounded-l-xl rounded-br-xl">
               <div>{{ messageList.content }}</div>
               <div class="text-[12px] opacity-60">{{ formatDate(new Date(messageList.createdAt),"hh:mm a") }}</div>
             </div>
           </div>
           <div class="message-right-align" v-if="messageList.role === 'assistant'">
-            <div>
-              <div class="flex items-center justify-center rounded-full ai-profile-align">
-                V
-              </div>
-            </div>
+            <span class="text-[14px]" style="color: #8A8A8A">{{ leadData?.bot.metadata.prompt.NAME }}</span>
             <div class="flex flex-col gap-2 ai-reply-align rounded-r-xl rounded-bl-xl">
-              <div>
-                <!-- {{ JSON.parse(messageList.content) }} -->
-                {{ JSON.parse(messageList.content).response }}
-              </div>
-              <div class="flex flex-col gap-4">
-                <!-- <div class="font-black">
-                  {{ JSON.parse(messageList.content).canned }}
-                  {{ JSON.parse(messageList.content).intents }}
-                </div> -->
+              <MdText :content="JSON.parse(messageList.content).response"/>
+              <div class="flex flex-col">
                 <div class="flex items-center gap-2">
                   <div class="flex items-center" v-for="(btn, btnIndex) in JSON.parse(messageList.content).canned"
                     :key="btnIndex">
-                    <Uibutton class="rounded-xl pricing-align-btn">{{ btn.title }}
-                    </Uibutton>
+                    <p class="rounded-xl pricing-align-btn">{{ btn.title }}
+                    </p>
                   </div>
                   <!-- <Uibutton class="rounded-xl pricing-align-btn">{{ JSON.parse(messageList.content).canned[0].title }}
                   </Uibutton> -->
@@ -116,15 +90,7 @@
 
 </template>
 <script setup lang="ts">
-import { Button } from '@/components/ui/button'
-// import { Input } from '@/components/ui/input'
-// import { Label } from '@/components/ui/label'
-import {
-  Tabs,
-  TabsContent,
-  TabsList,
-  TabsTrigger,
-} from '@/components/ui/tabs'
+import MdText from '~/components/MdText.vue';
 
   definePageMeta({
     middleware: "admin-only",
@@ -133,7 +99,13 @@ import {
   const router = useRouter();
   const route = useRoute();
   const paramId: any = route
-  const getLeadTranscriptList: any = await getLeadTranscript(paramId.params.id)
+  const leadData = await getLeadTranscript(paramId.params.id)
+
+  const details = computed(() => {
+    if(!leadData) return [undefined, undefined]
+    const { params,...rest} =  leadData.metadata as Record<string, any>
+    return [Object.entries(rest), Object.entries(params)]
+  })
 
 // const responseText = computed(() => {
 //   if (getLeadTranscriptList && getLeadTranscriptList[0].messages) {
@@ -188,7 +160,7 @@ import {
   text-underline-offset: 8px
 }
 
-.tab-align:focus {
+[data-active="true"] {
   color: rgba(66, 75, 209, 1);
   font-weight: 800;
   text-decoration: underline;
@@ -211,7 +183,7 @@ import {
 }
 
 .chatinfo-align {
-  width: 70%;
+  width: 50%;
   height: 75vh;
   background: rgba(255, 255, 255, 1);
   box-shadow: 0px 2px 24px 0px rgba(0, 0, 0, 0.05) !important;
@@ -221,7 +193,7 @@ import {
 .chat-header-align {
   width: 100%;
   height: 70px;
-  background: #EC848B;
+  background: #424bd1;
   color: white;
   padding: 0 20px;
 }
@@ -265,7 +237,7 @@ import {
 }
 
 .ai-profile-align {
-  background: #EC848B;
+  background: #424bd1;
   color: white;
   width: 50px;
   height: 50px;
@@ -284,11 +256,11 @@ import {
   padding: 10px;
 }
 .pricing-align-btn {
-  color: #EC848B;
+  color: #424bd1;
   width: auto;
   /* color: white; */
-  padding: 10px;
-  border: 1px solid #EC848B;
+  padding: 3% 10px;
+  border: 1px solid #424bd1;
 }
 .all-message-align {
   height: 65vh;
