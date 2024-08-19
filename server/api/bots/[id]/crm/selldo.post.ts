fde9dfdffaa367ca7b38275832ef2bc10591755d
Naveen G <naveeng2402@gmail.com>
2024-08-19 17:20:58 +0530

    feat: selldo integration

---
+++ server/api/bots/[id]/crm/selldo.post.ts
@@ -0,0 +1,52 @@
+export const createLeadInSellDo = async (
+  notes: string,
+  user: Record<string, any>,
+  analytics: Record<string, any>,
+) => {
+  const crmDetails = {
+    apiKey: "5c9507a0d2483722e200876b8d55a365",
+    campaignId: "",
+    projectId: "",
+  };
+
+  while (true) {
+    try {
+      const response: any = await $fetch(
+        `https://app.sell.do/api/leads/create/`,
+        {
+          method: "POST",
+          body: {
+            api_key: crmDetails.apiKey,
+            sell_do: {
+              form: {
+                lead: {
+                  name: user.name,
+                  email: user.email,
+                  phone: user.mobile,
+                  campaign_id: crmDetails.campaignId,
+                  source: "Tring Chatbot",
+                  sub_source: "Tring Chatbot",
+                  project_id: crmDetails.projectId,
+                },
+                note: {
+                  content: notes,
+                },
+              },
+              campaign: { srd: crmDetails.projectId },
+              analytics,
+            },
+          },
+        },
+      );
+
+      if (response.sell_do_lead_id.length > 0) return response.sell_do_lead_id;
+
+      console.log("Lead creation failed, retrying in 5 seconds...");
+      await sleep(5000);
+    } catch (e) {
+      console.error(e);
+      await sleep(5000);
+      continue;
+    }
+  }
+};
