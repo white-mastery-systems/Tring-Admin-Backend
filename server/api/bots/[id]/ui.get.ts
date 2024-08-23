e5b8621c3369ca97095c1555f2ca20c2d68e3f8e
john <johnbabu@whitemastery.com>
2024-08-23 10:49:01 +0530

    fix

--- server/api/bots/[id]/ui.get.ts
+++ server/api/bots/[id]/ui.get.ts
@@ -7,11 +7,11 @@ export default defineEventHandler(async (event) => {
   if (!bot) return sendError(event, createError({ statusCode: 404 }));
 
   const availableQuota = bot.organization.maxQuota - bot.organization.usedQuota;
-  // if (bot.organization.planCode === "FREE" && availableQuota <= 0)
-  //   return sendError(
-  //     event,
-  //     createError({ statusCode: 403, statusMessage: "Trial Expired" }),
-  //   );
+  if (bot.organization.planCode === "FREE" && availableQuota <= 0)
+    return sendError(
+      event,
+      createError({ statusCode: 403, statusMessage: "Trial Expired" }),
+    );
 
   return (bot.metadata as Record<string, any>).ui;
 });
