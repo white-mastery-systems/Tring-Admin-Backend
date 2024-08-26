296baed9430cb73a9c2573a7e657490d69261586
sathish <sathishsam78@gmail.com>
2024-08-26 13:28:33 +0530

    unwanted custom class has removed and design corrections

--- components/NavigationDrawer.vue
+++ components/NavigationDrawer.vue
@@ -1,58 +1,42 @@
 <template>
   <div
-    class="flex h-[100vh] w-[250px] flex-col items-center justify-center gap-5 bg-[#ffffff]"
-  >
+    class="flex h-[100vh] w-[250px] flex-col items-center justify-center gap-5 bg-[#ffffff] overflow-y-scroll pt-[80px] sm:pt-[80px] md:pt-[80px] lg:pt-0 xl:pt-0">
     <div class="flex h-[20vh]">
       <img src="assets\icons\tring_AI_logo.svg" width="80" height="80" />
     </div>
     <template v-for="{ name, icon, path } in navigationModules" :key="path">
-      <NuxtLink
-        v-if="!!(path !== '/')"
-        :to="path"
-        @click="handleNavigation"
+      <NuxtLink v-if="!!(path !== '/')" :to="path" @click="handleNavigation"
         class="field_shadow flex w-[90%] cursor-pointer items-center gap-3 rounded-[10px] px-[18px] py-4 font-medium sm:w-[90%] md:w-[80%] xl:w-[90%]"
         :class="[
           route.path?.includes(path)
             ? 'bg-[#424bd1] text-[#ffffff]'
             : 'bg-[#ffffff]',
-        ]"
-      >
+        ]">
         <component :is="icon"></component>
 
         <span class="text-[14px]">{{ name }}</span>
       </NuxtLink>
-      <NuxtLink
-        v-else
-        :to="path"
-        @click="handleNavigation"
+      <NuxtLink v-else :to="path" @click="handleNavigation"
         class="field_shadow flex w-[90%] cursor-pointer items-center gap-3 rounded-[10px] px-[18px] py-4 font-medium sm:w-[90%] md:w-[80%] xl:w-[90%]"
         :class="[
           route.path === path ? 'bg-[#424bd1] text-[#ffffff]' : 'bg-[#ffffff]',
-        ]"
-      >
+        ]">
         <!-- note i tried merge this if else didn't worked so did this way -->
         <component :is="icon"></component>
         <span class="text-[14px]">{{ name }}</span>
       </NuxtLink>
     </template>
-    <NuxtLink
-      class="flex w-[90%] items-center gap-2 rounded-xl border-[1px] border-[var(border)] px-2 py-1"
-      to="/account"
-      :class="[
+    <NuxtLink class="flex w-[90%] items-center gap-2 rounded-xl border-[1px] border-[var(border)] px-2 py-1"
+      to="/account" :class="[
         route.path === '/account'
           ? 'bg-[#424bd1] text-[#ffffff]'
           : 'bg-[#ffffff]',
-      ]"
-    >
+      ]">
       <UiAvatar>
-        <UiAvatarImage
-          class="capitalize"
-          :src="userInfo?.profile_image"
-          :alt="userInfo?.username"
-        />
+        <UiAvatarImage class="capitalize" :src="userInfo?.profile_image" :alt="userInfo?.username" />
         <UiAvatarFallback>{{
           userInfo?.username?.toUpperCase()?.charAt(0)
-        }}</UiAvatarFallback>
+          }}</UiAvatarFallback>
       </UiAvatar>
 
       <div class="flex flex-col">
@@ -61,20 +45,14 @@
       </div>
     </NuxtLink>
 
-    <UiButton
-      @click="confirmModel"
+    <UiButton @click="confirmModel"
       class="mb-8 mt-auto w-3/4 items-start justify-around bg-[#ffffff] pr-12 font-bold text-[#ff0000] hover:bg-gray-300/30 hover:text-[#ff0000] hover:brightness-110"
-      variant="ghost"
-    >
+      variant="ghost">
       <Icon name="ic:round-logout" class="h-6 w-6" />
       <p class="text-base">Logout</p>
     </UiButton>
-    <ConfirmationModal
-      v-model:open="modalOpen"
-      title="Confirm Logout"
-      description="Are you sure you want to log out ?"
-      @confirm="handleLogout"
-    />
+    <ConfirmationModal v-model:open="modalOpen" title="Confirm Logout" description="Are you sure you want to log out ?"
+      @confirm="handleLogout" />
     <!-- </div> -->
   </div>
 </template>
