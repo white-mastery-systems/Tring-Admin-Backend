f6b27be32897a9096aa6c8df75724271fdcb024b
john <johnbabu@whitemastery.com>
2024-08-31 09:53:03 +0530

    fix

---
+++ components/dashboard/StatusCountCard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div
+    class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3"
+  >
+    <div class="rounded-md bg-[#ffbc42] p-2">
+      <!-- <ChatSession /> -->
+      <component :is="props.icon"></component>
+    </div>
+    <div>
+      <div
+        class="mb-1 text-[10px] font-semibold text-gray-500 sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px]"
+      >
+        {{ props.title }}
+      </div>
+      <div
+        class="text-xl font-extrabold text-black sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl"
+      >
+        {{ props.count ?? 0 }}
+      </div>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+  const props = defineProps<{ title: string; count: string ,icon:any}>();
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,32 @@
 <template>
-  <Page :disable-elevation="true" title="Dashboard" :disableSelector="true" :disable-back-button="true">
+  <Page
+    :disable-elevation="true"
+    title="Dashboard"
+    :disableSelector="true"
+    :disable-back-button="true"
+  >
     <template #actionButtons>
-      <span class="flex items-center field_shadow rounded-lg text-[15px]" style="color: rgba(138, 138, 138, 1)">
+      <span
+        class="field_shadow flex items-center rounded-lg text-[15px]"
+        style="color: rgba(138, 138, 138, 1)"
+      >
         <!-- <span class="flex -items-center py-2 pl-2"></span> -->
         <span class="font-bold text-black">
           <UiSelect v-model="selectedValue" class="outline-none">
             <UiSelectTrigger
-              class="ui-select-trigger flex w-[70px] sm:w-[80px] md:w-[200px] lg:w-[200px] xl:w-[200px] items-center gap-2 outline-none text-[10px] sm:text-[10px] md:text-[14px] lg:text-[14px] xl:text-[14px]">
+              class="ui-select-trigger flex w-[70px] items-center gap-2 text-[10px] outline-none sm:w-[80px] sm:text-[10px] md:w-[200px] md:text-[14px] lg:w-[200px] lg:text-[14px] xl:w-[200px] xl:text-[14px]"
+            >
               <span class="font-thin text-gray-400"> Summary </span>
               <UiSelectValue />
             </UiSelectTrigger>
             <UiSelectContent>
               <UiSelectGroup>
-                <UiSelectItem v-for="(list, index) in menuList" :key="index" class="content_align pr-2"
-                  :value="list.value">
+                <UiSelectItem
+                  v-for="(list, index) in menuList"
+                  :key="index"
+                  class="content_align pr-2"
+                  :value="list.value"
+                >
                   {{ list.content }}
                 </UiSelectItem>
               </UiSelectGroup>
@@ -23,8 +36,13 @@
       </span>
     </template>
     <div>
-      <div class="xs:grid-cols-2 grid grid-cols-2 gap-6 md:grid-cols-2 lg:grid-cols-4">
-        <div v-if="false" class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
+      <div
+        class="xs:grid-cols-2 grid grid-cols-2 gap-6 md:grid-cols-2 lg:grid-cols-4"
+      >
+        <div
+          v-if="false"
+          class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3"
+        >
           <div class="rounded-md bg-[#ffbc42] p-2">
             <BotIcon />
           </div>
@@ -37,170 +55,84 @@
             </div>
           </div>
         </div>
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Interacted Chats
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.interactedChats[0]?.count ?? 0 }}
-            </div>
-          </div>
-        </div>
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Interacted Chats"
+          :count="analyticsData?.interactedChats[0]?.count"
+        />
 
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Chat sessions
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.chats }}
-            </div>
-          </div>
-        </div>
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Chat sessions"
+          :count="analyticsData?.chats"
+        />
 
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <Leads />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Chat Leads
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.leads }}
-            </div>
-          </div>
-        </div>
+        <StatusCountCard
+          :icon="Leads"
+          title="Chat Leads"
+          :count="analyticsData?.leads"
+        />
 
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <SingleUser />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Unique visitors
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.users }}
-            </div>
-          </div>
-        </div>
+        <StatusCountCard
+          :icon="SingleUser"
+          title="Unique visitors"
+          :count="analyticsData?.users"
+        />
 
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Call Scheduled
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.callScheduledTimeline[0]?.count ?? 0 }}
-            </div>
-          </div>
-        </div>
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Site Visits
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.siteVisitTimeline[0]?.count ?? 0 }}
-            </div>
-          </div>
-        </div>
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Virtual Tours
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.virtualTourTimeline[0]?.count ?? 0 }}
-            </div>
-          </div>
-        </div>
-        <div class="field_shadow flex items-center gap-6 rounded-[10px] bg-white px-2 py-3">
-          <div class="rounded-md bg-[#ffbc42] p-2">
-            <ChatSession />
-          </div>
-          <div>
-            <div
-              class="mb-1 text-[10px] sm:text-[10px] md:text-[16px] lg:text-[16px] xl:text-[16px] font-semibold text-gray-500">
-              Location Visited
-            </div>
-            <div class="text-xl sm:text-xl md:text-3xl lg:text-3xl xl:text-3xl font-extrabold text-black">
-              {{ analyticsData?.locationTimeline[0]?.count ?? 0 }}
-            </div>
-          </div>
-        </div>
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Call Scheduled"
+          :count="analyticsData?.callScheduledTimeline[0]?.count"
+        />
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Site Visits"
+          :count="analyticsData?.siteVisitTimeline[0]?.count"
+        />
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Virtual Tours"
+          :count="analyticsData?.virtualTourTimeline[0]?.count"
+        />
+        <StatusCountCard
+          :icon="ChatSession"
+          title="Location Visited"
+          :count="analyticsData?.locationTimeline[0]?.count"
+        />
       </div>
 
       <div class="relative">
-        <div class="field_shadow my-8 flex h-[59vh] gap-6 rounded-[10px] pb-[20px]">
-          <div v-if="analyticsData?.bots > 0" class="relative w-full place-content-center rounded-md bg-white">
-            <UiLineChart :data="lineGraphData" index="month" :categories="['Leads Created', 'Sessions Created']"
-              :colors="['#424bd1', '#ffbc42']" :show-grid-line="true" :show-tooltip="true" :margin="{ right: 20 }"
+        <div
+          class="field_shadow my-8 flex h-[59vh] gap-6 rounded-[10px] pb-[20px]"
+        >
+          <div class="relative w-full place-content-center rounded-md bg-white">
+            <UiLineChart
+              :data="lineGraphData"
+              index="month"
+              :categories="['Leads Created', 'Sessions Created']"
+              :colors="['#424bd1', '#ffbc42']"
+              :show-grid-line="true"
+              :show-tooltip="true"
+              :margin="{ right: 20 }"
               :y-formatter="
                 (tick: any) => {
                   return typeof tick === 'number'
                     ? `${new Intl.NumberFormat('us').format(tick).toString()}`
                     : '';
                 }
-              " class="h-[380px] w-full" />
-          </div>
-          <!-- <div
-            class="voice-bot-align relative place-content-center rounded-md bg-white shadow"
-          >
-            <UiLabel class="absolute top-6 pl-6 text-lg font-extrabold"
-              >Voice Bot</UiLabel
-            >
-            <VisBulletLegend
-              :items="chartDataItems"
-              class="absolute right-6 top-10 flex flex-col pt-3"
+              "
+              class="h-[380px] w-full"
             />
-            <UiDonutChart
-              index="name"
-              :category="'total'"
-              :data="chartsData"
-              :colors="['#ffbc42', '#424bd1']"
-              :arc-width="50"
-            >
-            </UiDonutChart>
-          </div> -->
+          </div>
         </div>
-        <!-- <UiLabel class="absolute right-1/3 top-1/3 z-10  -translate-x-3/4 -translate-y-1/3 text-lg">Getting Started by Creating Bots</UiLabel> -->
-        <!-- <UiButton
-          v-if="analyticsData?.bots === 0"
-          class="absolute right-1/2 top-1/2 z-10 h-16 w-56 -translate-y-1/2 translate-x-1/2 bg-[#474df9] text-lg text-white hover:bg-[#474df9] hover:brightness-90"
-          @click="getStarted"
-          >Get Started</UiButton
-        > -->
       </div>
     </div>
   </Page>
 </template>
 <script setup lang="ts">
+  import ChatSession from "~/components/icons/ChatSession.vue";
   import Leads from "~/components/icons/Leads.vue";
+  import SingleUser from "~/components/icons/SingleUser.vue";
 
   definePageMeta({
     middleware: "user",
