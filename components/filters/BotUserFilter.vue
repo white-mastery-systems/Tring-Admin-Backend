462107913680bff4462353a243df376d18f5e4b9
sathish <sathishsam78@gmail.com>
2024-09-03 16:59:58 +0530

    chat page search filter and select filter issue

---
+++ components/filters/BotUserFilter.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const emit = defineEmits(["changeAction"]);
+const selectedAction: any = ref('')
+const actionFilters = reactive([
+  {
+    content: "With Name",
+    value: "with_name",
+  }, {
+    content: "Without Name",
+    value: "without_name",
+  }
+]);
+
+watch(selectedAction, (newValue) => {
+  emit("changeAction", newValue);
+})
+</script>
+
+<template>
+  <UiSelect v-model="selectedAction">
+    <UiSelectTrigger class="w-[110px] sm:w-[110px] md:w-[250px] lg:w-[250px] xl:w-[250px]">
+      <UiSelectValue placeholder="Select a User" />
+    </UiSelectTrigger>
+    <UiSelectContent>
+      <UiSelectItem value="all">All</UiSelectItem>
+      <UiSelectItem v-for="(list, index) in actionFilters" :key="index" :value="list.value">
+        {{ list.content }}
+      </UiSelectItem>
+    </UiSelectContent>
+  </UiSelect>
+</template>
