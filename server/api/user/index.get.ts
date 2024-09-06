60a8d98517ce38b1c1bb8caa805c1dd0c0a5b886
Dhanalakshmi282 <dhanamcse282@gmail.com>
2024-09-06 10:17:31 +0530

    fix

--- server/api/user/index.get.ts
+++ server/api/user/index.get.ts
@@ -1,6 +1,5 @@
 export default defineEventHandler(async(event) => {
   const userId = event.context.user?.id as string
-  await isOrganizationAdminHandler(event);
 
   const userDetails = await getUserById(userId)
 
