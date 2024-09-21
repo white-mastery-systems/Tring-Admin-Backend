8067df00cafca9976055bf8a4a003c06ecb9b5a7
arvinthvikas <184011101024arvinthvikas@gmail.com>
2024-09-21 11:49:20 +0530

    user role integration completed

--- components/settings/Modals/RoleManageMentModal.vue
+++ components/settings/Modals/RoleManageMentModal.vue
@@ -1,22 +1,21 @@
 <script setup lang="ts">
   import { DateFormatter } from "@internationalized/date";
+  import Checkbox from "~/components/ui/checkbox/Checkbox.vue";
   import { roleMangementSchema } from "~/validationSchema/settings/roleManagementValidation.";
 
   definePageMeta({
     middleware: "admin-only",
   });
   const emit = defineEmits<{ (e: "confirm"): void }>();
-  const roleModalState  = defineModel<{ open: boolean; id: any }>({
+  const roleModalState = defineModel<{ open: boolean; id: any }>({
     default: {
       open: false,
       id: null,
     },
   });
 
-
-
   const campaignListWithLabels = computed(() => {
-    if (roleModalState ) {
+    if (roleModalState) {
       return userDataList.value?.map((item: any) => {
         return {
           value: item.id,
@@ -44,21 +43,19 @@
     validationSchema: roleMangementSchema,
   });
 
-
   watch(
-    () => roleModalState .value.open,
+    () => roleModalState.value.open,
     async (newState) => {
       resetForm();
-      if (roleModalState .value.id) {
+      if (roleModalState.value.id) {
         const getSingleDetails: any = await $fetch(
-          `/api/user-role/${roleModalState .value.id}`,
+          `/api/user-role/${roleModalState.value.id}`,
         );
-          Object.entries(getSingleDetails).forEach(([key, value]: any) => {
-            if (values.hasOwnProperty(key)) {
-              setFieldValue(key, value);
-            }
-          });
-
+        Object.entries(getSingleDetails).forEach(([key, value]: any) => {
+          if (values.hasOwnProperty(key)) {
+            setFieldValue(key, value);
+          }
+        });
       }
     },
   );
@@ -66,48 +63,60 @@
   const handleConnect = handleSubmit(async (values: any) => {
     try {
       if (roleModalState.value.id) {
-        await $fetch(`/api/user-role/${roleModalState .value.id}`, {
+        await $fetch(`/api/user-role/${roleModalState.value.id}`, {
           method: "PUT",
           body: values,
         });
         toast.success("Updated successfully");
       } else {
-        await $fetch("/api/user-role", { method: "POST", body: {...values,permissions:{
-          sendEmail:true
-        }} });
+        await $fetch("/api/user-role", {
+          method: "POST",
+          body: {
+            ...values,
+            permissions: {
+              sendEmail: true,
+            },
+          },
+        });
         toast.success("Created successfully");
       }
-      emit("confirm")
+      emit("confirm");
     } catch (error: any) {
       toast.error(error.statusMessage);
     }
   });
-
-
 </script>
 <template>
   <DialogWrapper
-    v-model="roleModalState "
-    :title="roleModalState .id ? 'Modify Role' : 'Add Role'"
+    v-model="roleModalState"
+    :title="roleModalState.id ? 'Modify Role' : 'Add Role'"
   >
-      <form class="space-y-2" @submit="handleConnect">
-        <div class="grid gap-4">
-          <TextField
-            type="name"
-            name="name"
-            label="Name"
-            placeholder="Enter Your Role"
-            :required="true"
-          />
+    <form class="space-y-2" @submit="handleConnect">
+      <div class="grid gap-4">
+        <TextField
+          type="name"
+          name="name"
+          label="Name"
+          placeholder="Enter Your Role"
+          :required="true"
+        />
 
-          
-
-          <div class="flex w-full justify-end">
-            <UiButton type="submit" class="mt-2" color="primary">
-              Submit
-            </UiButton>
-          </div>
+        <div>
+          <Checkbox id="terms" />
+          <label
+            for="terms"
+            class="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+          >
+            Email Config
+          </label>
         </div>
-      </form>
+
+        <div class="flex w-full justify-end">
+          <UiButton type="submit" class="mt-2" color="primary">
+            Submit
+          </UiButton>
+        </div>
+      </div>
+    </form>
   </DialogWrapper>
 </template>
