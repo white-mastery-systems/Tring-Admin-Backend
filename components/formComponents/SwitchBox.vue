54711cf6c3c1be21024c84ef15f313fafd662c05
arvinthvikas <184011101024arvinthvikas@gmail.com>
2024-09-24 18:36:14 +0530

    implement range slider

---
+++ components/formComponents/SwitchBox.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Label } from '@/components/ui/label'
+import { Switch } from '@/components/ui/switch'
+</script>
+
+<template>
+  <div class="flex items-center space-x-2">
+    <Switch id="airplane-mode" />
+    <Label for="airplane-mode">Airplane Mode</Label>
+  </div>
+</template>
