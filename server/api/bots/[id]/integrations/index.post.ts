60fbede36b077b8bdb3be1f0a7f9f01d509f2768
arvinthvikas <184011101024arvinthvikas@gmail.com>
2024-10-23 11:30:32 +0530

    update

--- server/api/bots/[id]/integrations/index.post.ts
+++ server/api/bots/[id]/integrations/index.post.ts
@@ -8,7 +8,6 @@ export const zodInsertBotIntegration = z.object({
   pipelineObj: z.any().optional(),
   channelId: z.string().optional(),
   layoutObj: z.any().optional(),
-  stage: z.string().optional()
 });
 
 export default defineEventHandler(async (event) => {
