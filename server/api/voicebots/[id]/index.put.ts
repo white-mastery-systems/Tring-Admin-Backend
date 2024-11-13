19a5dfa35825fafc0f2f2468924a9c66407d67ca
Dhanalakshmi282 <dhanamcse282@gmail.com>
2024-11-13 15:43:22 +0530

    voicebot table changes

--- server/api/voicebots/[id]/index.put.ts
+++ server/api/voicebots/[id]/index.put.ts
@@ -8,41 +8,24 @@ const zodUpdateVoiceBotSchema = z.object({
   domain: z.array(z.string()).optional(),
   active: z.boolean().optional(),
   metaData: z.record(z.any()).optional(), // Assuming metaData is a JSON object with any structure
-  llmConfig: z
-    .object({
-      provider: z.string().optional(),
-      model: z.string().optional(),
-      tokens: z.string().optional(),
-      temperature: z.number().optional(),
-      documentId: z.string().optional(),
-      role: z.string().optional(),
-      guide: z.string().optional(),
-      instruction: z.string().optional(),
-      notes: z.string().optional(),
-      domainRules: z.string().optional(),
-      prompt: z.string()
-    })
-    .optional(),
-  identityManagement: z
-    .object({
-      name: z.string().optional(),
-      role: z.string().optional(),
-      domain: z.string().optional(),
-      other: z.string().optional(),
-    })
-    .optional(),
-  talentConfig: z
-    .object({
-      name: z.string().optional(),
-      type: z.string().optional(),
-      size: z.number().optional(),
-      url: z.string().optional(),
-    })
-    .optional(),
+  llmConfig: z.record(z.any()).optional(),
+  identityManagement: z.record(z.any()).optional(),
+  botDetails: z.object({
+    agentName: z.string().optional(),
+    agentLanguage: z.string().optional()
+  }).optional(),
+  preRecordedAudios: z.object({
+    welcomeAudio: z.string().optional(),
+    concludeAudio: z.string().optional()
+  }).optional(),
+  clientConfig: z.object({
+    llmCaching: z.boolean().optional(),
+    dynamicCaching: z.boolean().optional(),
+    distance: z.string().optional()
+  }).optional(),
   audioFiles: z.record(z.any()).optional(),
   textToSpeechConfig: z.record(z.any()).optional(),
   speechToTextConfig: z.record(z.any()).optional(),
-  intents: z.array(z.string()).optional(),
   ivrConfig: z.string().optional(),
 });
 
