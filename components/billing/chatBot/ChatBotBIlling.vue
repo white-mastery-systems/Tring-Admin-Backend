ae7889d9df29c69d8bc70a1e1f9c31c6a9f35063
Purushothaman R <purushothamanramesh3@gmail.com>
2024-11-16 18:09:56 +0530

    fix

--- components/billing/chatBot/ChatBotBIlling.vue
+++ components/billing/chatBot/ChatBotBIlling.vue
@@ -1,58 +1,77 @@
 <template>
   <div
-    class="mt-4 mb-[120px] sm:mb-[120px] md:mb-0 lg:mb-0 xl:mb-0 w-full self-center rounded-lg bg-[#fffff] shadow-3xl">
+    class="mb-[120px] mt-4 w-full self-center rounded-lg bg-[#fffff] shadow-3xl sm:mb-[120px] md:mb-0 lg:mb-0 xl:mb-0"
+  >
     <div
-      class="flex items-center justify-between rounded-t-xl border-b-[1px] border-[#80808036] px-[5px] py-5 text-[18px] font-bold sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+      class="flex items-center justify-between rounded-t-xl border-b-[1px] border-[#80808036] px-[5px] py-5 text-[18px] font-bold sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+    >
       <span> Subscription Details </span>
     </div>
-    <!-- class="overflow-y-scroll h-screen-minus-15" -->
     <div>
       <div
-        class="flex items-center justify-between gap-3 px-[5px] py-5 sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 px-[5px] py-5 sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span class="font-medium"> Current plan </span>
         <div class="flex min-w-[80px] items-center justify-start">
-          <span class="rounded-[11px] bg-[#d9dbe6] px-2.5 py-[3px] text-[12px] font-medium capitalize text-[#00000080]">
+          <span
+            class="rounded-[11px] bg-[#d9dbe6] px-2.5 py-[3px] text-[12px] font-medium capitalize text-[#00000080]"
+          >
             {{ usage?.plan_code?.replaceAll("_", " ")?.replace("chat", "") }}
           </span>
         </div>
       </div>
       <div
-        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span> Subscription status </span>
         <div class="flex min-w-[80px] items-center justify-start">
-          <span class="rounded-[11px] bg-[#3cb3714d] px-2.5 py-[3px] text-[12px] capitalize text-[#008026ba]">
+          <span
+            class="rounded-[11px] bg-[#3cb3714d] px-2.5 py-[3px] text-[12px] capitalize text-[#008026ba]"
+          >
             active
           </span>
         </div>
       </div>
 
       <div
-        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span> Total chat sessions used </span>
-        <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
+        <span
+          class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+        >
           {{ Number(usageDetails?.chatsUsedInPlan) }}
         </span>
       </div>
       <div
-        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span>Chat sessions included in your plan</span>
-        <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
+        <span
+          class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+        >
           {{ usageDetails?.planSessions }}
         </span>
       </div>
 
       <div
-        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span> Balance chat sessions available in your plan </span>
-        <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
+        <span
+          class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+        >
           {{ usageDetails?.chatsAvailableInPlan }}
         </span>
       </div>
 
       <div
-        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span> Extra chat sessions used</span>
-        <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
+        <span
+          class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+        >
           {{ usageDetails?.extraChatsMade }}
         </span>
       </div>
@@ -67,26 +86,33 @@
           </span>
         </div> -->
       <div
-        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+        class="flex items-center justify-between gap-3 px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span>Chat sessions available in wallet</span>
         <div class="flex min-w-[80px] items-center justify-start">
-          <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
+          <span
+            class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+          >
             {{
-            Number(usageDetails?.individualChatsCost) === 0
-            ? 0
-            : (Math.floor(
-            Number(usageDetails?.walletBalance) /
-            Number(usageDetails?.individualChatsCost),
-            ) ?? 0)
+              Number(usageDetails?.individualChatsCost) === 0
+                ? 0
+                : (Math.floor(
+                    Number(usageDetails?.walletBalance) /
+                      Number(usageDetails?.individualChatsCost),
+                  ) ?? 0)
             }}
           </span>
         </div>
       </div>
-      <div v-if="usageDetails?.expiryDate"
-        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]">
+      <div
+        v-if="usageDetails?.expiryDate"
+        class="flex items-center justify-between gap-3 rounded-b-lg px-[5px] py-5 font-medium sm:px-[5px] md:px-[30px] lg:px-[30px] xl:px-[30px]"
+      >
         <span> Billing Expiry Date </span>
-        <span class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]">
-          {{format(new Date(usageDetails?.expiryDate), "MMMM d, yyyy")}}
+        <span
+          class="flex min-w-[80px] items-center justify-start rounded-xl text-[15px]"
+        >
+          {{ format(new Date(usageDetails?.expiryDate), "MMMM d, yyyy") }}
         </span>
       </div>
     </div>
@@ -94,11 +120,11 @@
 </template>
 
 <script setup lang="ts">
-import { format } from "date-fns";
+  import { format } from "date-fns";
 
-const props = defineProps({
-  usageDetails: Object,
-  subscriptionData: Object,
-  usage: Object,
-});
+  const props = defineProps({
+    usageDetails: Object,
+    subscriptionData: Object,
+    usage: Object,
+  });
 </script>
