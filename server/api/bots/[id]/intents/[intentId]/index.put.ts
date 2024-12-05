f5fb2760c4f45b99dec0ba93528cd5608d41b9fe
Dhanalakshmi282 <dhanamcse282@gmail.com>
2024-12-05 18:03:06 +0530

    feat: email recipients configuration for intents

--- server/api/bots/[id]/intents/[intentId]/index.put.ts
+++ server/api/bots/[id]/intents/[intentId]/index.put.ts
@@ -1,5 +1,22 @@
 import { updateBotIntent } from "~/server/utils/db/bot";
-import { zodInsertChatBotIntent } from "../index.post";
+// import { zodInsertChatBotIntent } from "../index.post";
+
+const zodIntentUpdate = z.object({
+  intent: z.string().optional(),
+  emailRecipients: z.array(z.string()).optional(),
+  isEmailEnabled: z.boolean().optional(),
+  link: z.string().url().min(5, "Link too short").optional(),
+}).refine((data) => {
+  // Require `link` only when intent is "location" or "virtual_tour"
+  if (["location", "virtual_tour"].includes(data?.intent)) {
+    return !!data.link; 
+  }
+  return true;
+}, {
+  message: "Link is required for 'location' and 'virtual_tour' intents",
+  path: ["link"],
+});
+
 
 export default defineEventHandler(async (event) => {
     await isOrganizationAdminHandler(event); 
@@ -9,7 +26,7 @@ export default defineEventHandler(async (event) => {
      );
     const { intentId } = await isValidRouteParamHandler( event, checkPayloadId("intentId"));
 
-    const intent: any = await isValidBodyHandler(event, zodInsertChatBotIntent);
+    const intent: any = await isValidBodyHandler(event, zodIntentUpdate);
 
     const updateIntent = await updateBotIntent(botId, intentId, intent);
 
