a638892d991177d734c6c25d03c3bb7af6da98b0
Dhanalakshmi282 <dhanamcse282@gmail.com>
2024-12-23 11:19:43 +0530

    voicebot - ivr config changes

--- server/utils/drizzle.ts
+++ server/utils/drizzle.ts
@@ -2,7 +2,7 @@ import { drizzle } from "drizzle-orm/node-postgres";
 import pg from "pg";
 import { contactListAndContactsRelations, contactListContactsSchema, orgSubscriptionSchema, playgroundDocumentSchema, voicebotContactSchema, voiceBucketContactsRelations, voiceContactLinkSchema } from "../schema/admin";
 import { userOTPSchema } from "../schema/auth";
-import { callLogsRelations, outboundCallSchema, voicebotIntegrationRelations, voicebotRelations } from "../schema/voicebot";
+import { callLogsRelations, outboundCallSchema, voicebotIntegrationRelations, voicebotRelations, voicebotSchedularSchema } from "../schema/voicebot";
 import { botDynamicFormSchema } from "../schema/bot";
 
 const runtimeConfig = useRuntimeConfig();
@@ -47,6 +47,7 @@ const schema = {
   outboundCallSchema,
   voicebotContactSchema,
   voiceContactLinkSchema,
+  voicebotSchedularSchema,
   // Relations
   organizationRelations,
   chatBotRelations,
