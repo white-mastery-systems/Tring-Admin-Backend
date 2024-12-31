58d01b5004f5341032fae615919b2826c03af6f5
sathish <sathishsam78@gmail.com>
2024-12-31 10:44:21 +0530

    call logs and chats page tab blank setup and chat bot configure channel Integration field optional setup has completed

--- pages/analytics/chats/index.vue
+++ pages/analytics/chats/index.vue
@@ -22,209 +22,204 @@
         <CountryFilter @changeCountry="onCountryChange"></CountryFilter>
       </div>
     </div>
-    <DataTable @row-click="
-        (row: any) => {
-          return navigateTo(`/analytics/chats/${row.original.id}`);
-        }
-      " @pagination="Pagination" @limit="
-        ($event) => {
-          (filters.page = '1'), (filters.limit = $event);
-        }
+    <DataTable @row-click="handleRowClick" @pagination="Pagination" @limit="($event) => {
+        (filters.page = '1'), (filters.limit = $event);
+      }
       " :totalPageCount="totalPageCount" :page="page" :totalCount="totalCount" :columns="columns" :data="chats"
       :page-size="20" :is-loading="isDataLoading" :height="13" height-unit="vh" />
   </Page>
 </template>
 <script setup lang="ts">
-  import { createColumnHelper } from "@tanstack/vue-table";
-  import { format } from "date-fns";
-  definePageMeta({
-    middleware: "user",
-  });
-  useHead({
-    title: "Analytics | Chats",
-  });
-  const formSchema = toTypedSchema(
-    z.object({
-      newBotName: z.string().min(2, "Bot Name is requird."),
-    }),
-  );
-  const searchBot = ref("");
-  // const searchBotDebounce = refDebounced(searchBot, 500);
+import { createColumnHelper } from "@tanstack/vue-table";
+import { format } from "date-fns";
+definePageMeta({
+  middleware: "user",
+});
+useHead({
+  title: "Analytics | Chats",
+});
+const formSchema = toTypedSchema(
+  z.object({
+    newBotName: z.string().min(2, "Bot Name is requird."),
+  }),
+);
+const searchBot = ref("");
+// const searchBotDebounce = refDebounced(searchBot, 500);
 
-  const activeStatus = ref("");
+const activeStatus = ref("");
 
-  watch(activeStatus, async (newStatus, previousStatus) => {});
+watch(activeStatus, async (newStatus, previousStatus) => { });
 
-  const filters = reactive<{
-    botId: string;
-    q?: string;
-    from?: string;
-    to?: string;
-    botUserName: string;
-    period: string;
-    page: string;
-    limit: string;
-    channel: any;
-    country: string;
-  }>({
-    botId: "",
-    q: undefined,
-    from: undefined,
-    to: undefined,
-    botUserName: "all",
-    period: "all-time",
-    page: "1",
-    limit: "10",
-    channel: "all",
-    country: "all",
-  });
-  const exportDataHandler = ref({ status: false, type: "csv" });
+const filters = reactive<{
+  botId: string;
+  q?: string;
+  from?: string;
+  to?: string;
+  botUserName: string;
+  period: string;
+  page: string;
+  limit: string;
+  channel: any;
+  country: string;
+}>({
+  botId: "",
+  q: undefined,
+  from: undefined,
+  to: undefined,
+  botUserName: "all",
+  period: "all-time",
+  page: "1",
+  limit: "10",
+  channel: "all",
+  country: "all",
+});
+const exportDataHandler = ref({ status: false, type: "csv" });
 
-  let page = ref(0);
-  let totalPageCount = ref(0);
-  let totalCount = ref(0);
-  const fetchExportData = ref(false);
-  const {
-    status,
-    data: chats,
-    refresh: getAllChats,
-  } = await useLazyFetch("/api/chats?page=2&limit=8", {
-    server: false,
-    query: filters,
-    default: () => [],
-    headers: {
-      "time-zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+let page = ref(0);
+let totalPageCount = ref(0);
+let totalCount = ref(0);
+const fetchExportData = ref(false);
+const {
+  status,
+  data: chats,
+  refresh: getAllChats,
+} = await useLazyFetch("/api/chats?page=2&limit=8", {
+  server: false,
+  query: filters,
+  default: () => [],
+  headers: {
+    "time-zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+  },
+  transform: (chats: any) => {
+    page.value = chats.page;
+    totalPageCount.value = chats.totalPageCount;
+    totalCount.value = chats.totalCount;
+    return chats?.data?.map((chat: any) => ({
+      userName: chat.botUser?.name || "No name",
+      email: chat?.botUser?.email,
+      mobile: chat?.botUser?.mobile,
+      countryCode: chat?.botUser?.countryCode,
+      id: chat.id,
+      location: `${chat.metadata?.city ?? "--"} - ${chat.metadata?.state ?? "--"} `,
+      updatedAt: `${chat?.updatedAt}`,
+      metadata: chat?.metadata,
+      mode: chat?.mode,
+      channel: chat?.channel,
+      botUser: chat.bot?.name,
+    }));
+  },
+});
+
+const exportFilters = computed(() => {
+  const { page, limit, ...restFilters } = filters; // Destructure to exclude 'page' and 'limit'
+  return restFilters;
+});
+const exportReadyRows = ref<any>([]);
+// const exportReadyRows = computed(() => {
+//   let formatExportChats: any;
+//   if (Array.isArray(exportChats.value)) {
+//     formatExportChats = toRaw(exportChats.value); // Convert to plain array
+//   } else if (exportChats.value && typeof exportChats.value === "object") {
+//     formatExportChats = toRaw(exportChats.value.data) || []; // Convert to plain object or array
+//   }
+
+//   try {
+//     return formatExportChats.map((chat: any) => {
+//       const mergedObject = {
+//         name: chat?.botUser?.name ?? "---",
+//         email: chat?.botUser?.email ?? "---",
+//         countryCode: chat?.botUser?.countryCode ?? "+91",
+//         mobile: chat?.botUser?.mobile ?? "---",
+//         botName: chat?.bot?.name ?? "---",
+//         country: chat?.metadata?.country ?? "---",
+//         createdAt: format(chat?.createdAt, "MMMM d, yyyy"),
+//         channel: chat?.channel,
+//         mode: chat?.mode,
+//       };
+//       return mergedObject;
+//     });
+//   } catch (err) {
+//     console.log({ err });
+//   }
+// });
+const exportReadyColumns = computed(() => {
+  return [
+    "Name",
+    "Email",
+    "Country Code",
+    "Mobile",
+    // "Visited status",
+    "Bot name",
+    "Country",
+    "Updated At",
+    "Channel",
+    "Mode",
+  ];
+});
+
+const isDataLoading = computed(() => {
+  console.log(status.value, "VALUE STATUS");
+  return status.value === "pending";
+});
+
+const statusComponent = (status: boolean) =>
+  status
+    ? h("span", { class: "text-green-500" }, "Active")
+    : h("span", { class: "text-red-500" }, "Inactive");
+
+const columnHelper = createColumnHelper<(typeof chats.value)[0]>();
+const columns = [
+  columnHelper.accessor("userName", {
+    header: "User Name",
+  }),
+  columnHelper.accessor("location", {
+    header: "Location",
+  }),
+  columnHelper.accessor("mode", {
+    header: "Mode",
+    cell: (props) => {
+      return h(
+        "span",
+        {
+          class: `${props.row.original.mode === "preview" ? "text-red-500" : "text-green-500"}`,
+        },
+        props.row.original.mode,
+      );
     },
-    transform: (chats: any) => {
-      page.value = chats.page;
-      totalPageCount.value = chats.totalPageCount;
-      totalCount.value = chats.totalCount;
-      return chats?.data?.map((chat: any) => ({
-        userName: chat.botUser?.name || "No name",
-        email: chat?.botUser?.email,
-        mobile: chat?.botUser?.mobile,
-        countryCode: chat?.botUser?.countryCode,
-        id: chat.id,
-        location: `${chat.metadata?.city ?? "--"} - ${chat.metadata?.state ?? "--"} `,
-        updatedAt: `${chat?.updatedAt}`,
-        metadata: chat?.metadata,
-        mode: chat?.mode,
-        channel: chat?.channel,
-        botUser: chat.bot?.name,
-      }));
-    },
-  });
-
-  const exportFilters = computed(() => {
-    const { page, limit, ...restFilters } = filters; // Destructure to exclude 'page' and 'limit'
-    return restFilters;
-  });
-  const exportReadyRows = ref<any>([]);
-  // const exportReadyRows = computed(() => {
-  //   let formatExportChats: any;
-  //   if (Array.isArray(exportChats.value)) {
-  //     formatExportChats = toRaw(exportChats.value); // Convert to plain array
-  //   } else if (exportChats.value && typeof exportChats.value === "object") {
-  //     formatExportChats = toRaw(exportChats.value.data) || []; // Convert to plain object or array
-  //   }
-
-  //   try {
-  //     return formatExportChats.map((chat: any) => {
-  //       const mergedObject = {
-  //         name: chat?.botUser?.name ?? "---",
-  //         email: chat?.botUser?.email ?? "---",
-  //         countryCode: chat?.botUser?.countryCode ?? "+91",
-  //         mobile: chat?.botUser?.mobile ?? "---",
-  //         botName: chat?.bot?.name ?? "---",
-  //         country: chat?.metadata?.country ?? "---",
-  //         createdAt: format(chat?.createdAt, "MMMM d, yyyy"),
-  //         channel: chat?.channel,
-  //         mode: chat?.mode,
-  //       };
-  //       return mergedObject;
-  //     });
-  //   } catch (err) {
-  //     console.log({ err });
-  //   }
-  // });
-  const exportReadyColumns = computed(() => {
-    return [
-      "Name",
-      "Email",
-      "Country Code",
-      "Mobile",
-      // "Visited status",
-      "Bot name",
-      "Country",
-      "Updated At",
-      "Channel",
-      "Mode",
-    ];
-  });
-
-  const isDataLoading = computed(() => {
-    console.log(status.value, "VALUE STATUS");
-    return status.value === "pending";
-  });
-
-  const statusComponent = (status: boolean) =>
-    status
-      ? h("span", { class: "text-green-500" }, "Active")
-      : h("span", { class: "text-red-500" }, "Inactive");
-
-  const columnHelper = createColumnHelper<(typeof chats.value)[0]>();
-  const columns = [
-    columnHelper.accessor("userName", {
-      header: "User Name",
-    }),
-    columnHelper.accessor("location", {
-      header: "Location",
-    }),
-    columnHelper.accessor("mode", {
-      header: "Mode",
-      cell: (props) => {
-        return h(
-          "span",
-          {
-            class: `${props.row.original.mode === "preview" ? "text-red-500" : "text-green-500"}`,
-          },
-          props.row.original.mode,
-        );
-      },
-    }),
-    columnHelper.accessor("channel", {
-      header: "Channel",
-    }),
-    columnHelper.accessor("botUser", {
+  }),
+  columnHelper.accessor("channel", {
+    header: "Channel",
+  }),
+  columnHelper.accessor("botUser", {
     header: "Bot Name",
   }),
-    columnHelper.accessor("metadata.country", {
-      header: "Country",
-      cell: (info) => info.getValue() || "-",
-    }),
-    columnHelper.accessor("updatedAt", {
-      header: "Updated At",
-    }),
-  ];
-  const onActionChange = (value: any) => {
-    filters.botUserName = value;
-    filters.page = "1";
-  };
-  const Pagination = async ($evnt: any) => {
-    filters.page = $evnt;
-    getAllChats();
-  };
-  const onDateChange = (value: any) => {
-    if (value.from && value.to) {
-      filters.from = value.from;
-      filters.to = value.to;
-    } else {
-      delete filters.from;
-      delete filters.to;
-      filters.period = value;
-    }
-    filters.page = "1";
-  };
+  columnHelper.accessor("metadata.country", {
+    header: "Country",
+    cell: (info) => info.getValue() || "-",
+  }),
+  columnHelper.accessor("updatedAt", {
+    header: "Updated At",
+  }),
+];
+const onActionChange = (value: any) => {
+  filters.botUserName = value;
+  filters.page = "1";
+};
+const Pagination = async ($evnt: any) => {
+  filters.page = $evnt;
+  getAllChats();
+};
+const onDateChange = (value: any) => {
+  if (value.from && value.to) {
+    filters.from = value.from;
+    filters.to = value.to;
+  } else {
+    delete filters.from;
+    delete filters.to;
+    filters.period = value;
+  }
+  filters.page = "1";
+};
 
 const onBotChange = (value: any) => {
   if (value) {
@@ -232,47 +227,50 @@ const onBotChange = (value: any) => {
     filters.page = '1'
   }
 };
-  const onChannelChange = ($event) => {
-    if ($event) {
-      filters.channel = $event;
+const onChannelChange = ($event) => {
+  if ($event) {
+    filters.channel = $event;
+  }
+};
+const onCountryChange = ($event) => {
+  if ($event) {
+    filters.country = $event;
+  }
+};
+const handleRowClick = (row) => {
+  window.open(`/analytics/chats/${row.original.id}`, '_blank');
+}
+const exportData = async () => {
+  try {
+    const exportChats = await $fetch("/api/chats", {
+      query: exportFilters.value,
+      method: "GET",
+      headers: {
+        "time-zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+    });
+    let formatExportChats: any;
+    if (Array.isArray(exportChats)) {
+      formatExportChats = toRaw(exportChats); // Convert to plain array
+    } else if (exportChats && typeof exportChats === "object") {
+      formatExportChats = toRaw(exportChats.data) || []; // Convert to plain object or array
     }
-  };
-  const onCountryChange = ($event) => {
-    if ($event) {
-      filters.country = $event;
-    }
-  };
-  const exportData = async () => {
-    try {
-      const exportChats = await $fetch("/api/chats", {
-        query: exportFilters.value,
-        method: "GET",
-        headers: {
-          "time-zone": Intl.DateTimeFormat().resolvedOptions().timeZone,
-        },
-      });
-      let formatExportChats: any;
-      if (Array.isArray(exportChats)) {
-        formatExportChats = toRaw(exportChats); // Convert to plain array
-      } else if (exportChats && typeof exportChats === "object") {
-        formatExportChats = toRaw(exportChats.data) || []; // Convert to plain object or array
-      }
-      const exportReadObject = formatExportChats.map((chat: any) => {
-        const mergedObject = {
-          name: chat?.botUser?.name ?? "---",
-          email: chat?.botUser?.email ?? "---",
-          countryCode: chat?.botUser?.countryCode ?? "+91",
-          mobile: chat?.botUser?.mobile ?? "---",
-          botName: chat?.bot?.name ?? "---",
-          country: chat?.metadata?.country ?? "---",
-          updatedAt: format(chat?.updatedAt, "MMMM d, yyyy"),
-          channel: chat?.channel,
-          mode: chat?.mode,
-        };
-        return mergedObject;
-      });
-      exportDataHandler.value.status = true;
-      exportReadyRows.value = exportReadObject;
-    } catch (err) {}
-  };
+    const exportReadObject = formatExportChats.map((chat: any) => {
+      const mergedObject = {
+        name: chat?.botUser?.name ?? "---",
+        email: chat?.botUser?.email ?? "---",
+        countryCode: chat?.botUser?.countryCode ?? "+91",
+        mobile: chat?.botUser?.mobile ?? "---",
+        botName: chat?.bot?.name ?? "---",
+        country: chat?.metadata?.country ?? "---",
+        updatedAt: format(chat?.updatedAt, "MMMM d, yyyy"),
+        channel: chat?.channel,
+        mode: chat?.mode,
+      };
+      return mergedObject;
+    });
+    exportDataHandler.value.status = true;
+    exportReadyRows.value = exportReadObject;
+  } catch (err) { }
+};
 </script>
