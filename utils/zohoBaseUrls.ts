85bcb3a18b125a2fe330970341560a1f619bf901
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-02-10 10:39:16 +0530

    feat: refactor Zoho-CRM API integrations to use centralized base URLs

---
+++ utils/zohoBaseUrls.ts
@@ -0,0 +1,23 @@
+export const zohoAuthApiBaseUrls: any = {
+  eu: "https://accounts.zoho.eu",
+  ae: "https://accounts.zoho.ae",
+  au: "https://accounts.zoho.com.au",
+  in: "https://accounts.zoho.in",
+  jp: "https://accounts.zoho.jp",
+  uk: "https://accounts.zoho.uk",
+  us: "https://accounts.zoho.com",
+  ca: "https://accounts.zohocloud.ca",
+  sa: "https://accounts.zoho.sa",
+};
+
+export const zohoIntegrationApiBaseUrls: any = {
+  eu: "https://www.zohoapis.eu",
+  ae: "https://www.zohoapis.ae",
+  au: "https://www.zohoapis.com.au",
+  in: "https://www.zohoapis.in",
+  jp: "https://www.zohoapis.jp",
+  uk: "https://www.zohoapis.uk",
+  us: "https://www.zohoapis.com",
+  ca: "https://www.zohoapis.ca",
+  sa: "https://www.zohoapis.sa",
+}
