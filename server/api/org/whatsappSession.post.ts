f223f153e18c721a4e24a1c1e1a33efc17f2ad64
GJDeepak <gjdeepak784@gmail.com>
2025-03-20 13:03:54 +0530

    enhance WhatsApp session handling and add revisiting field

--- server/api/org/whatsappSession.post.ts
+++ server/api/org/whatsappSession.post.ts
@@ -1,3 +1,4 @@
+import { logger } from "~/server/logger";
 import { createOrgWhatsappSession, getOrgWhatsappSessions } from "~/server/utils/db/whatsappSessions";
 
 export default defineEventHandler(async (event) => {
@@ -8,47 +9,50 @@ export default defineEventHandler(async (event) => {
     countryCode: z.string(),
     integrationId: z.string()
   }))
-  const { organizationId } = body
+  try {
+    const { organizationId, pid, mobile } = body
+    
+    const [orgZohoSubscription, orgDetail, orgPlanUsage] = await Promise.all([
+       getOrgZohoSubscription(organizationId, "chat"),
+       getOrganizationById(organizationId),
+       getOrgPlanUsage(organizationId, "chat")
+    ])
+    
+    let whatsappWalletBalance = orgDetail?.wallet || 0
   
-  const [orgZohoSubscription, orgDetail, orgPlanUsage] = await Promise.all([
-     getOrgZohoSubscription(organizationId, "chat"),
-     getOrganizationById(organizationId),
-     getOrgPlanUsage(organizationId, "chat")
-  ])
-  
-  let whatsappWalletBalance = orgDetail?.wallet || 0
-
-  if(orgZohoSubscription?.subscriptionStatus !== "active" && whatsappWalletBalance <= 0) {
-    return{ status: false, whatsappWalletBalance, organizationName: orgDetail?.name }
-  }
- 
-  const whatsappSessionPrice = parseFloat((1 * 1.5).toFixed(2))
-  whatsappWalletBalance = Math.max(0, parseFloat((whatsappWalletBalance - whatsappSessionPrice).toFixed(2)));
-
-  const whatsappSessionExist = await getOrgWhatsappSessions(organizationId, body.pid, body.mobile)
-
-  if (whatsappSessionExist) {
-    const createdAt = new Date(whatsappSessionExist.createdAt);
-    const now = new Date();
-    const hoursDifference = (now.getTime() - createdAt.getTime()) / (1000 * 60 * 60); // hours
-  
-    if (hoursDifference > 2) {
-      await createOrgWhatsappSession(body)
-      await updateOrganization(organizationId, { wallet: whatsappWalletBalance })
-      await updateSubscriptionPlanUsage(
-        orgPlanUsage?.id!,
-        { interactionsUsed: (orgPlanUsage?.interactionsUsed || 0) + 1 }
-      )
-      return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name }
+    if(orgZohoSubscription?.subscriptionStatus !== "active" && whatsappWalletBalance < 1.5) {
+      return { status: false, whatsappWalletBalance, organizationName: orgDetail?.name, revisited: false }
     }
-      return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name }
-  } else {
-    await createOrgWhatsappSession(body)
-    await updateOrganization(organizationId, { wallet: whatsappWalletBalance })
-    await updateSubscriptionPlanUsage(
-      orgPlanUsage?.id!,
-      { interactionsUsed: (orgPlanUsage?.interactionsUsed || 0) + 1 }
-    )
-    return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name }
+   
+    const whatsappSessionPrice = parseFloat((1 * 1.5).toFixed(2))
+    whatsappWalletBalance = Math.max(0, parseFloat((whatsappWalletBalance - whatsappSessionPrice).toFixed(2)));
+  
+    const whatsappSessionExist = await getOrgWhatsappSessions(organizationId, pid, mobile)
+  
+    if (whatsappSessionExist) {
+      const createdAt = new Date(whatsappSessionExist.createdAt);
+      const now = new Date();
+      const hoursDifference = (now.getTime() - createdAt.getTime()) / (1000 * 60 * 60); // hours
+    
+      if (hoursDifference > 2) {
+        await Promise.all([
+          createOrgWhatsappSession(body),
+          updateOrganization(organizationId, { wallet: whatsappWalletBalance }),
+          updateSubscriptionPlanUsage(orgPlanUsage?.id!, { interactionsUsed: (orgPlanUsage?.interactionsUsed || 0) + 1 })
+        ])
+        return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name, revisited: true }
+      }
+        return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name, revisited: false }
+    } else {
+      await Promise.all([
+        createOrgWhatsappSession(body),
+        updateOrganization(organizationId, { wallet: whatsappWalletBalance }),
+        updateSubscriptionPlanUsage(orgPlanUsage?.id!, { interactionsUsed: (orgPlanUsage?.interactionsUsed || 0) + 1 })
+      ])
+      return { status: true, whatsappWalletBalance, organizationName: orgDetail?.name, revisited: false }
+    }
+  } catch (error:any) {
+    logger.error(`Whatsapp Session Error: ${error?.message}`)
+    return { status: true, revisited: false };
   }
 })
