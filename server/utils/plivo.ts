ed2c6fd4f34fa1a54803f63dea5c5ccdbc9e6b09
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-03-21 18:47:20 +0530

    feat: add multiple IVR integration and Plivo phone number purchase functionality

--- server/utils/plivo.ts
+++ server/utils/plivo.ts
@@ -42,9 +42,32 @@ export const listPlivoSubAccountPhoneNumbers = async ({ subAccountAuthId, subAcc
     if(!data.objects) {
       throw new Error("data not found")
     }
-    return data?.objects
+    const phoneNumbers = data?.objects.map((i: any)=> i.number)
+    return phoneNumbers
   } catch (error: any) {
-    logger.error(`listPlivoSubAccountPhoneNumbers function Error: ${JSON.stringify(error.message)}`)
+    logger.error(`ListPlivoSubAccountPhoneNumbers function Error: ${JSON.stringify(error.message)}`)
+    throw new Error(error)
+  }
+}
+
+export const buyPhoneNumberForPlivoSubAccount = async ({ subAccountAuthId, phoneNumber }: {
+  subAccountAuthId: string,
+  phoneNumber: string
+}) => {
+  try {
+    const credentials = btoa(`${config.tringPlivoAuthId}:${config.tringPlivoAuthToken}`);
+    const data = await $fetch(`https://api.plivo.com/v1/Account/${config.tringPlivoAuthId}/PhoneNumber/${phoneNumber}/`, {
+      method: "POST",
+      headers: {
+        Authorization: `Basic ${credentials}`,
+      },
+      body: {
+        subaccount: subAccountAuthId
+      }
+    })
+    return data
+  } catch (error: any) {
+    logger.error(`Buy phoneNumber for plivo sub-account function Error: ${JSON.stringify(error.message)}`)
     throw new Error(error)
   }
 }
