90a07c2d5560994505ca5453f414af04d8205545
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-04-02 09:58:56 +0530

    feat: update createTicket API to include orgId and admin details; streamline ticket payload construction

--- server/api/zoho-desk/createTicket.post.ts
+++ server/api/zoho-desk/createTicket.post.ts
@@ -3,15 +3,11 @@ import { errorResponse } from "~/server/response/error.response"
 import { createTicketInZohoDesk, getZohoDeskApiKey } from "~/server/utils/v2/integrations/crm/zoho/zoho-desk"
 
 const zodCreateTicketValidation = z.object({
+  orgId: z.string(),
   botId: z.string(),
   type: z.enum(["voice", "chat"]),
   subject: z.string(),
-  departmentId: z.string(),
-  email: z.string(),
-  firstName: z.string(),
-  lastName: z.string(),
-  phone: z.string(),
-  description: z.string().optional(),
+  description: z.string(),
   category: z.string().optional(),
   subCategory: z.string().optional(),
   resolution: z.string().optional(),
@@ -26,12 +22,22 @@ export default defineEventHandler(async (event) => {
   try {
     const body = await isValidBodyHandler(event, zodCreateTicketValidation)
 
+    const adminDetails = await getAdminByOrgId(body?.orgId)
+
     const integrationData = await getZohoDeskApiKey({ botId: body.botId, type: body.type })
     if(!integrationData.status) {
       return errorResponse(event, 400, integrationData.message)
     }
-    const { botId, type, ...payload}  = body
-    const data = await createTicketInZohoDesk({ integrationData: integrationData.data, body: payload })
+    const { botId, type, ...payload }  = body
+    const ticketPayload = {
+      ...payload,
+      departmentId: "191068000000280029",
+      email: adminDetails?.email,
+      firstName: adminDetails?.username,
+      phone: `${adminDetails?.countryCode} ${adminDetails?.mobile}`
+    };
+
+    const data = await createTicketInZohoDesk({ integrationData: integrationData.data, body: ticketPayload })
     if(!data.status) {
       return errorResponse(event, 500, "Unable to create ticket in zoho-desk")
     }
