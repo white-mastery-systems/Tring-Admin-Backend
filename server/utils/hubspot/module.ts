9dc05655366da704b519bc57a64794e95bb4c3b6
GJDeepak <gjdeepak784@gmail.com>
2025-04-15 10:52:38 +0530

    refactor: remove commented-out HubSpot event data for cleaner code

--- server/utils/hubspot/module.ts
+++ server/utils/hubspot/module.ts
@@ -1,27 +1,3 @@
-/*[ { eventId: 3764650983,
-    subscriptionId: 3443675,
-    portalId: 242485166,
-    appId: 4035002,
-    occurredAt: 1744372157861,
-    subscriptionType: 'deal.creation',
-    attemptNumber: 3,
-    objectId: 64261714667,
-    changeFlag: 'CREATED',
-    changeSource: 'INTEGRATION',
-    sourceId: '4035002' },
-  { eventId: 3540261506,
-    subscriptionId: 3443687,
-    portalId: 242485166,
-    appId: 4035002,
-    occurredAt: 1744372157861,
-    subscriptionType: 'object.creation',
-    attemptNumber: 3,
-    objectId: 64261714667,
-    objectTypeId: '0-3',
-    changeFlag: 'CREATED',
-    changeSource: 'INTEGRATION',
-    sourceId: '4035002' } ]*/
-
 import { getBotIntegrationByOrgId } from "../db/bot";
 
 export const findHubspotEventNames = (event: any) => {
@@ -50,7 +26,7 @@ export const sendMeetingLink = async (link:any, organizationId:any)=>{
     const metadata = botIntegrationData?.metadata || null;
     const integrationData = botIntegrationData?.integration || null;
     if(metadata && integrationData){
-      
+
     }
   } catch (error:any) {
     console.log(error.message);
