3caa8c43e35624769a896ef2dbdd134b88b6f280
GJDeepak <gjdeepak784@gmail.com>
2025-04-25 17:33:32 +0530

    refactor: enhance user enrichment logic and update meeting reminder message format

--- server/plugin/calendly-scheduled-events.ts
+++ server/plugin/calendly-scheduled-events.ts
@@ -1,6 +1,7 @@
+import { format } from "date-fns";
 import { logger } from "../logger"
 import * as schedule from "node-schedule"
-import { getAllScheduledEventInvitees, getAllCalendlyScheduledEvents, getParsedPhoneNumber } from "~/server/utils/calently/module";
+import { getAllScheduledEventInvitees, getAllCalendlyScheduledEvents, getParsedPhoneNumber, getTimeBasedCalendlyScheduledEvents } from "~/server/utils/calently/module";
 import { getEnrichByEmailOrPhone, updateWhatsappEnrichStatusById, bookedWhatsappEnrichList } from "~/server/utils/db/whatsapp-enrichment";
 
 export default defineNitroPlugin(async (event) => {
@@ -32,16 +33,75 @@ export default defineNitroPlugin(async (event) => {
             await Promise.all(inviteeList.map(async (invitee: any) => {
               const inviteeId = invitee?.uri.split("/invitees/")[1];
               const inviteeDetails = invitee;
+              let enrichUser = await getEnrichByEmailOrPhone(inviteeDetails?.email)
               if (inviteeDetails && inviteeDetails?.email && inviteeDetails?.questions_and_answers[0]?.answer) {
                 const { phone } = getParsedPhoneNumber(inviteeDetails?.questions_and_answers[0]?.answer);
-                const userDetails = await getEnrichByEmailOrPhone(inviteeDetails?.email, phone);
-                if (userDetails && !["meeting_booked", "completed"].includes(userDetails.status)) {
-                  const enrichStatus = (userDetails.status === "meeting_cancelled") ? "meeting_rescheduled" : "meeting_booked";
+                enrichUser = await getEnrichByEmailOrPhone(inviteeDetails?.email, phone);
+              }
+              const userDetails = enrichUser;
+              if (userDetails && !["meeting_booked", "completed"].includes(userDetails.status)) {
+                const enrichStatus = (userDetails.status === "meeting_cancelled") ? "meeting_rescheduled" : "meeting_booked";
 
-                  if (userDetails.metadata && !userDetails.metadata?.bookedMessage) {
+                // @ts-ignore
+                if (userDetails.metadata && !userDetails.metadata?.bookedMessage) {
+                  const userPhone =`${userDetails.botUser.countryCode}${userDetails.botUser.mobile}`.replace("+", "");
+                  const metadata = userDetails.integration.metadata;
+                  const message = `Your meeting slot is confirmed \n\nClick meeting link below: \nLink: ${schEvents?.location?.join_url}`;
+                  await Promise.all([
+                    // @ts-ignore
+                    sendWhatsappMessage(metadata?.access_token, metadata?.pid, userPhone, message),
+                    updateWhatsappEnrichStatusById(userDetails.id, enrichStatus, { ...(userDetails.metadata || {}), inviteeId, bookedMessage: true, link: schEvents?.location?.join_url}),
+                  ]);
+                } else {
+                  await updateWhatsappEnrichStatusById(userDetails.id, enrichStatus, {...(userDetails.metadata || {}), inviteeId, bookedMessage: true, link: schEvents?.location?.join_url});
+                }
+              }
+
+              const startTimeUTC = new Date(schEvents?.start_time); // 6:30 PM IST
+              const now = new Date(); // current time in IST system time
+              const isPassed = now > startTimeUTC;
+
+              if (userDetails && ["meeting_booked", "meeting_rescheduled"].includes(userDetails.status) && isPassed) {
+                await updateWhatsappEnrichStatusById(userDetails.id, "completed", {...(userDetails.metadata || {}), inviteeId, bookedMessage: true });
+              }
+            }));
+          }
+        }));
+      }
+    });
+
+    schedule.scheduleJob("*/30 * * * *", async () => {
+      logger.info("Calendly scheduled event notification cron job started");
+      const now = new Date(); // current time in IST system time
+      const tomorrow = new Date(now);
+      tomorrow.setDate(now.getDate() + 1);
+
+      const start = tomorrow.toISOString();
+      const scheduledEvents = await getTimeBasedCalendlyScheduledEvents(undefined, undefined, "active", start);
+
+      if(scheduledEvents.length){
+        await Promise.all(scheduledEvents.map(async (schEvents: any) => {
+          const inviteeList = await getAllScheduledEventInvitees(null, schEvents?.uri);
+          if (inviteeList.length) {
+            await Promise.all(inviteeList.map(async (invitee: any) => {
+              const inviteeId = invitee?.uri.split("/invitees/")[1];
+              const inviteeDetails = invitee;
+              
+              let enrichUser = await getEnrichByEmailOrPhone(inviteeDetails?.email)
+              if (inviteeDetails && inviteeDetails?.email && inviteeDetails?.questions_and_answers[0]?.answer) {
+                const { phone } = getParsedPhoneNumber(inviteeDetails?.questions_and_answers[0]?.answer);
+                enrichUser = await getEnrichByEmailOrPhone(inviteeDetails?.email, phone);
+              }
+              const userDetails = enrichUser;
+              if (userDetails && !["meeting_booked", "completed"].includes(userDetails.status)) {
+                  const enrichStatus = (userDetails.status === "meeting_cancelled") ? "meeting_rescheduled" : "meeting_booked";
+                  // @ts-ignore
+                  if (userDetails.metadata) {  
                     const userPhone =`${userDetails.botUser.countryCode}${userDetails.botUser.mobile}`.replace("+", "");
                     const metadata = userDetails.integration.metadata;
-                    const message = `Your meeting slot is confirmed \n\nClick meeting link below: \nLink: ${schEvents?.location?.join_url}`;
+                    const meetingTime = format(new Date(schEvents.start_time), "hh:mm a"); // optional formatting
+                    const message = `Hi *${userDetails.name || inviteeDetails?.name || ""}*! 👋\n\nJust a reminder that you have a meeting scheduled *today at ${meetingTime}*.\n\nClick the link below to join:\n🔗 ${schEvents?.location?.join_url}\n\nLooking forward to connecting!`;
+                    console.log("message", message, { link:schEvents?.location?.join_url, userPhone, token:metadata?.access_token, pid:metadata?.pid});
                     await Promise.all([
                       // @ts-ignore
                       sendWhatsappMessage(metadata?.access_token, metadata?.pid, userPhone, message),
@@ -59,28 +119,9 @@ export default defineNitroPlugin(async (event) => {
                 if (userDetails && ["meeting_booked", "meeting_rescheduled"].includes(userDetails.status) && isPassed) {
                   await updateWhatsappEnrichStatusById(userDetails.id, "completed", {...(userDetails.metadata || {}), inviteeId, bookedMessage: true });
                 }
-              }
             }));
           }
-        }));
-      }
-    });
-
-    // schedule.scheduleJob("*/11 * * * *", async () => {
-    schedule.scheduleJob("0 11 */2 * *", async () => {
-      logger.info("Calendly scheduled event cron job started");
-
-      const bookedEnrichList = await bookedWhatsappEnrichList();
-      if (bookedEnrichList.length) {
-        await Promise.all(bookedEnrichList.map(async (userDetails: any) => {
-          if (userDetails && ["meeting_booked", "meeting_rescheduled"].includes(userDetails.status)) {
-            const userPhone =`${userDetails.botUser.countryCode}${userDetails.botUser.mobile}`.replace("+", "");
-            const metadata = userDetails.integration.metadata;
-            const message = (userDetails.metadata?.link)? `Your meeting slot is confirmed \n\nClick meeting link below: \nLink: ${userDetails.metadata?.link}`:"Reminder you have meeting";
-
-            await sendWhatsappMessage(metadata?.access_token, metadata?.pid, userPhone, message);
-          }
-        }));
+        }))
       }
     });
   } catch (error: any) {
