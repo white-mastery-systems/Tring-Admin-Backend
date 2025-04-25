a84f8e6280a8ef4e28ac07eeff74126d106ebf6d
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-04-25 10:04:26 +0530

    refactor: rename and implement insertCallLogsInClay function for call log insertion

--- server/utils/clay/webhook.ts
+++ server/utils/clay/webhook.ts
@@ -10,7 +10,21 @@ export const insertRecordsInClay = async ({ body } : { body: any }) => {
     })
     return data
   } catch (error: any) {
-    logger.error(`Insert Records In Clay In Clay: ${JSON.stringify(error.message)}`)
+    logger.error(`Insert Records in Clay: ${JSON.stringify(error.message)}`)
+    throw new Error(error)
+  }
+}
+
+export const insertCallLogsInClay = async ({ body } : { body: any }) => {
+  try {
+    const data = await $fetch(`https://api.clay.com/v3/sources/webhook/pull-in-data-from-a-webhook-c5a30595-0f9c-4824-ad7a-b028fe0b383f`,
+    {
+      method: "POST",
+      body
+    })
+    return data
+  } catch (error: any) {
+    logger.error(`Insert Call-logs in Clay: ${JSON.stringify(error.message)}`)
     throw new Error(error)
   }
 }
