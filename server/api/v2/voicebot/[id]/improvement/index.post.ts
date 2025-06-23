19a2679a9b452aee7bbe9612383ea51c409c7f92
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-06-23 18:35:05 +0530

    Refactor voicebot improvement handler to process multiple items in the request body

--- server/api/v2/voicebot/[id]/improvement/index.post.ts
+++ server/api/v2/voicebot/[id]/improvement/index.post.ts
@@ -2,36 +2,36 @@ import { logger } from "~/server/logger"
 import { errorResponse } from "~/server/response/error.response"
 import { createVoicebotImprovementQueries, updateVoicebotImprovementQueries } from "~/server/utils/v2/db/voicebot"
 
-const zodVoicebotImprovementSchema = z.object({
-  id: z.string().optional(),
-  title: z.string(),
-  instances: z.array(z.any()),
-  suggestions: z.array(z.string())
-})
+const zodVoicebotImprovementSchema = z.array(
+  z.object({
+    id: z.string().optional(),
+    title: z.string(),
+    instances: z.array(z.any()),
+    suggestions: z.array(z.string())
+}))
 
 export default defineEventHandler(async (event) => {
   try {
     const { id: voicebotId } = await isValidRouteParamHandler(event, checkPayloadId("id"))
 
     const body = await isValidBodyHandler(event, zodVoicebotImprovementSchema)
-     
     const voicebotDetail = await getVoicebotById(voicebotId)
 
-    const voicebotResponseImprovement = {
-      ...body,
-      botId: voicebotId,
-      organizationId: voicebotDetail?.organizationId!,
+    for (const item of body) {
+      const payload = {
+        ...item,
+        botId: voicebotId,
+        organizationId: voicebotDetail?.organizationId!,
+      };
+
+      if (item?.id) {
+        await updateVoicebotImprovementQueries(item.id, payload)
+      } else {
+        await createVoicebotImprovementQueries(payload)
+      }
     }
 
-    if(voicebotResponseImprovement?.id) {
-      const updatedQueries = await updateVoicebotImprovementQueries(voicebotResponseImprovement.id, voicebotResponseImprovement)
-      return updatedQueries
-    }
-
-    const createBotQueries = await createVoicebotImprovementQueries(voicebotResponseImprovement)
-
-    return createBotQueries
-
+    return true
   } catch (error: any) {
     logger.error(`Voicebot improvement API Error: ${JSON.stringify(error.message)}`)
     return errorResponse(event, 500, "Unable to update the improvement for voicebot")
