43bacc1899ffb9b3dfbc9b5055063b2b2c2d2c5e
Purushothaman R <purushothamanramesh3@gmail.com>
2025-08-07 10:26:10 +0530

    test api

---
+++ server/api/getWeather.get.ts
@@ -0,0 +1,76 @@
+export default defineEventHandler(() => {
+  return {
+    weather_data: [
+      {
+        city: "New York",
+        temperature_celsius: 26,
+        humidity_percent: 60,
+        wind_kph: 15,
+        condition: "Partly Cloudy",
+      },
+      {
+        city: "London",
+        temperature_celsius: 18,
+        humidity_percent: 72,
+        wind_kph: 10,
+        condition: "Rainy",
+      },
+      {
+        city: "Tokyo",
+        temperature_celsius: 30,
+        humidity_percent: 65,
+        wind_kph: 20,
+        condition: "Sunny",
+      },
+      {
+        city: "Paris",
+        temperature_celsius: 22,
+        humidity_percent: 55,
+        wind_kph: 12,
+        condition: "Cloudy",
+      },
+      {
+        city: "Sydney",
+        temperature_celsius: 25,
+        humidity_percent: 50,
+        wind_kph: 18,
+        condition: "Clear",
+      },
+      {
+        city: "Mumbai",
+        temperature_celsius: 33,
+        humidity_percent: 75,
+        wind_kph: 25,
+        condition: "Thunderstorms",
+      },
+      {
+        city: "Cairo",
+        temperature_celsius: 35,
+        humidity_percent: 20,
+        wind_kph: 22,
+        condition: "Hot and Dry",
+      },
+      {
+        city: "Moscow",
+        temperature_celsius: 16,
+        humidity_percent: 70,
+        wind_kph: 14,
+        condition: "Overcast",
+      },
+      {
+        city: "Rio de Janeiro",
+        temperature_celsius: 28,
+        humidity_percent: 80,
+        wind_kph: 19,
+        condition: "Humid and Sunny",
+      },
+      {
+        city: "Cape Town",
+        temperature_celsius: 21,
+        humidity_percent: 58,
+        wind_kph: 13,
+        condition: "Windy",
+      },
+    ],
+  };
+});
