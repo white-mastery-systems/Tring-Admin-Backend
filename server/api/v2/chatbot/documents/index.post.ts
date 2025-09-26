5e770dcb0e5e9c47ab986963039add7e3ab06f09
Dhanalakshmi282 <dhanamcse282@gmail.com>
2025-09-26 18:05:24 +0530

    feat: authorization changes for chatbot creation

--- server/api/v2/chatbot/documents/index.post.ts
+++ server/api/v2/chatbot/documents/index.post.ts
@@ -1,16 +1,20 @@
+import { query } from "winston"
 import { logger } from "~/server/logger"
 import { errorResponse } from "~/server/response/error.response"
 import { uploadChatbotDocument } from "~/server/utils/db/document"
 
 export default defineEventHandler(async (event) => { 
   try {
-    const organizationId = await isOrganizationAdminHandler(event)
-
-    // Data Validation
+     // Data Validation
     const formData = await readMultipartFormData(event);
-   
+    
     if (!formData) {
       return errorResponse(event, 400, "Invalid Document Data: No form data was provided. Please ensure all required fields are included in the request.")
+    }
+
+    const organizationId = event?.context?.user?.organizationId ?? formData.find(({ name }) => name === "organizationId")?.data.toString()
+    if(!organizationId) {
+       return errorResponse(event, 401, 'UnAuthorized')
     }
     
     const fileData = formData.find(({ name }) => name === "files");
@@ -18,7 +22,7 @@ export default defineEventHandler(async (event) => {
     if (!fileData?.data) {
       return errorResponse(event, 400, "Invalid Document Data (files): The 'files' field is missing or invalid. Please ensure a valid file is provided.")
     }
-    
+
     const document = await uploadChatbotDocument(organizationId, fileData, fileName)
 
     return document
